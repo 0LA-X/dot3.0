ff76623ee95e63d0f68a91114d8b4f6d1c4eb160
0LA-X <165085183+0LA-X@users.noreply.github.com>
2025-10-10 18:14:16 +0100

    modified Size, Color & Cursor

--- Configs/.config/nvim/lua/options.lua
+++ Configs/.config/nvim/lua/options.lua
@@ -7,7 +7,18 @@ vim.cmd("set shiftwidth=2")
 
 local o = vim.opt
 -- add yours here!
+o.termguicolors = true  -- Enable true color support
+o.guicursor = "i-n-c-sm:block-blinkwait700-blinkon400-blinkoff250"
 
--- o.cursorlineopt ='both' -- to enable cursorline!
+--
+-- CursorLine
+o.cursorline = true
+o.cursorlineopt ='both' -- to enable cursorline!
+vim.cmd([[
+  highlight CursorLine ctermbg=DarkGrey guibg=#2a2a2a
+  highlight CursorLineNr ctermbg=DarkGrey guibg=#2a2a2a
+]])
+--
+--
 o.number = true         -- Show absolute line number
 o.relativenumber = true -- Show relative numbers
