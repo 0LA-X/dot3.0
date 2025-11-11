3ee56c0d88f3e30cda5e1c429a356f3c18921252
0LA-X <165085183+0LA-X@users.noreply.github.com>
2025-11-11 12:34:16 +0100

    modified: Enforced 2-space indentation

--- Configs/.config/nvim/lua/options.lua
+++ Configs/.config/nvim/lua/options.lua
@@ -5,6 +5,17 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+-- Enforce 2-space indents on all filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
 local o = vim.opt
 -- add yours here!
 o.termguicolors = true  -- Enable true color support
